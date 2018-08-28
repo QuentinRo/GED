e69753cbd6c29dc27fc4f0e812d340ac4a3886af
QuentinRo <31761186+QuentinRo@users.noreply.github.com>
2018-08-28 11:52:38 +0200

    Corrections de bug et finalisation du processus

--- traitement.php
+++ traitement.php
@@ -8,29 +8,33 @@
  */
 	// Test si le fichier à bien été envoyé  et sans erreur
 	// https://openclassrooms.com/fr/courses/918836-concevez-votre-site-web-avec-php-et-mysql/913099-transmettre-des-donnees-avec-les-formulaires
-	if (isset($_FILE['file']) AND $_FILE['file']['error'] == 0)
+
+	// AND isset($FILES['fichier']['webkitRelativePath'])Pour le chemin du fichier
+	if (isset($_FILES['fichier']['name']) AND isset($_FILES['fichier']['size']) )
 	{
+	
+		$name = $_FILES['fichier']['name'];
+		$size = $_FILES['fichier']['size'];
+	
+	}
+
+	if (isset($_POST['Option']) )
+	{
+	
+		$option = $_POST['Option'];
+	
+	}
 
 		//récuperer les infos du formulaire
 
 		//récuperer les infos du fichier
-		
-		$size == $_FILES['file']['size']; // selectionna la taille du fichier 
-		$name == $_FILES['file']['name']; // selectionna le nom du fichier
-
 
 		//sauvegarder le fichier
 		// https://developer.mozilla.org/fr/docs/Web/HTML/Element/Input/file
 		$handle =fopen("data.csv", "a+");
-		$list = array($name,'Cat','Name','Link',$size);
+		$list = array($name,$option,$size);
 		// Je n'ai pas de tableau multiple, il faut voir si je ne peut pas le faire sans foreach -----> fputcsv($handle, fields);
-		foreach ($list as $fields)
-		{
-			fputcsv($handle, $fields)
-		}
+		fputcsv($handle, $list);
 
-		//retourner sur la page avec un petit message d'erreur ou non 
-
-	}
-
-?>
+		header("location: index.php");
+	
