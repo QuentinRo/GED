deb43555b398f599660648beca1e71cb5a8c293c
QuentinRo <31761186+QuentinRo@users.noreply.github.com>
2018-08-29 09:22:12 +0200

    Dernier Push
    Rendu finale de l'application

--- index.php
+++ index.php
@@ -7,24 +7,24 @@
   <script src="script.js"></script>
   <!-- 
 	Auteur 					: Quentin Rossier
-	Version					: 0.1
+	Version					: 0.2
 	Date de création		: 28.08.2018	
-	Date de modification 	: 28.08.2018
+	Date de modification 	: 29.08.2018
 	Description : Importation et exportation de fichier
   -->
 </head>
 <body>
 
-	<form action="traitement.php" method="post" enctype="multipart/form-data"  onsubmit="this.filepath.value=this.fichier.value">
+	<form action="traitement.php" method="post" enctype="multipart/form-data">
 		<div>
 			<title> </title>
-			<LABEL name="Title">Titre</LABEL>
-			<input type="text" name="essais">
+			<LABEL name="Title" placeholder="Titre du document">Titre</LABEL>
+			<input type="text" name="Name">
 		</div>
 
 
 		<div>
-			<LABEL name="Cat">Categorie</LABEL>
+			<LABEL name="Cat" >Categorie</LABEL>
 			<SELECT name="Option">
 				<OPTION>Sport
 				<OPTION>Etude
@@ -35,23 +35,28 @@
 		<div>
 			<LABEL>Sélectionner un fichier</LABEL>
 			<INPUT type="file" name="fichier">
-			<input type="hidden" name="filepath">
 		</div>
 
 		<div>
 			<BUTTON>Télécharger</BUTTON>
 		</div>
 	</form>
+	<br>
 
 <?php 
+//lecture du fichier csv et affichage dans un tableau
 	$row = 1;
 	if (($handle = fopen("data.csv","r")) !== FALSE) 
 		{
+			echo "<table>
+			<td>Titre
+			<td>Catégorie
+			<td>Taille";
 			while(($data = fgetcsv($handle,1000, ",")) !==FALSE)
 			{
 				$num = count($data);
 				$row++;
-				echo "<table>";
+				echo "<tr>";
 				for ($c=0; $c < $num; $c++) 
 				{
 					echo "
@@ -59,9 +64,11 @@
 						";
 
 				}
-				echo "</table>";
+				echo "<tr>";
+				
 			}
 			fclose($handle);
+			echo "</table>";
 		}
 
 ?>
@@ -69,27 +76,8 @@
 	Difficulté 1 enctype="multipart/form-data"
 	Difficulté 2 crée et lire le fichier csv
 		simple d'utilisation et rappelle des fonctions
+	Dif 3, récupérer le chemin du fichier pour pouvoir le télécharger.
 	
-	
-
-	 // lecture du CSV et affichage de toutes les données (plus qu'à faire le tableau)
-	
-		 $row = 1;
-		 if (($handle = fopen("data.csv","r")) !== FALSE) 
-		{
-			while(($data = fgetcsv($handle,1000, ",")) !==FALSE)
-			{
-				$num = count($data);
-				echo "<p>" $num champs à la ligne $row : <br /></p>\n";
-				$row++;
-				for ($c=0; $c < $num; $c++) 
-				{
-					echo $data[$c] . "<br />\n";
-				}
-			}
-			fclose($handle);
-		}
-	 ?>
  -->
 </body>
 </html>
--- traitement.php
+++ traitement.php
@@ -1,77 +1,31 @@
 <?php 
 /*
 	Auteur 					: Quentin Rossier
-	Version					: 0.1
+	Version					: 0.2
 	Date de création		: 28.08.2018	
-	Date de modification 	: 28.08.2018
-	Description : écriture du fichier envoyé dans un CSV
-
-	solution à voir https://www.developpez.net/forums/d271948/php/langage/fichiers/input-file-recuperation-chemin/
+	Date de modification 	: 29.08.2018
+	Description : Récupération des données du formulaire et écriture du fichier envoyé dans un CSV
  */
 	// Test si le fichier à bien été envoyé  et sans erreur
-	// https://openclassrooms.com/fr/courses/918836-concevez-votre-site-web-avec-php-et-mysql/913099-transmettre-des-donnees-avec-les-formulaires
 
-	// AND isset($FILES['fichier']['webkitRelativePath'])Pour le chemin du fichier
-	if (isset($_FILES['fichier']['name']) AND isset($_FILES['fichier']['size']) AND isset($_FILES['fichier']['tmp_name']) )
+	if (isset($_FILES['fichier']['size']) AND isset($_FILES['fichier']['tmp_name']) )
 	{
 	
-		$name = $_FILES['fichier']['name'];
 		$size = $_FILES['fichier']['size'];
 
 		$link = $_FILES['fichier']['tmp_name'];
 
 	}
 
-	if (isset($_POST['filepath']))
+	if (isset($_POST['Option']) AND isset($_POST['Name']) )
 	{
-		echo $_POST['filepath'];
-	}
-
-	if (isset($_POST['Option']) )
-	{
-	
+		$name = $_POST['Name']; 
 		$option = $_POST['Option'];
-	
 	}
 
-	// a href"link" dowload = pour avoir le lien clicable mais il faut récuperer le truc et retirer le "/" visiblement
-	//manque d'attraper le lien dans le post
-		//récuperer les infos du formulaire
+	// ouverture du ficher à la fin, et écriture de la list d'information
+	$handle =fopen("data.csv", "a+");
+	$list = array($name,$option,$size ." K");
+	fputcsv($handle, $list);
 
-		//récuperer les infos du fichier
-
-		//sauvegarder le fichier
-		// https://developer.mozilla.org/fr/docs/Web/HTML/Element/Input/file
-		$handle =fopen("data.csv", "a+");
-		$list = array($name,$option,$size,$link);
-		// Je n'ai pas de tableau multiple, il faut voir si je ne peut pas le faire sans foreach -----> fputcsv($handle, fields);
-		fputcsv($handle, $list);
-
-		// header("location: index.php");
-	
-
-	/* solution javascripte ? solution en flash visiblement 
-		https://www.sitepoint.com/community/t/getting-the-full-path-with-input-type-file/1690/5
-		
-fkieber
-SitePoint Member
-Sep '04
-Hi,
-
-I search also an anser to this question. An because i have found nothing i have created a javascript that can be usefull.
-
-Here it is.
-
-<form method="what you want" enctype="do not use multipart/form-data to avoid transmission of the file contents to the server">
-<input type="hidden" name="full_path_real">
-<input name="full_path_fake" type="file" onchange='document.forms[0].elements["full_path_real"].value=document.forms[0].elements["full_path_fake"].value'>
-<input value="Go" type="submit">
-</form>
-
-Now use $REQUEST['fullpath_real'] and forgot $REQUEST['fullpath_fake'];
-
-That's all folks for all javascript able internet clients.
-
-Hope it was what you want. In my case i am happy.
-
-
+	header("location: index.php");
